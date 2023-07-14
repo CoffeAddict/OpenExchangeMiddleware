c6069a67aafeae89dcc33f68ee396b0b7e22b76d
Andres Rodriguez <andresrodriguezesteban@gmail.com>
2023-07-14 09:49:49 -0300

    added docs & historic endpoint

--- app.js
+++ app.js
@@ -10,20 +10,26 @@ dotenv.config()
 const port = process.env.PORT || 3000
 const apiKEY = process.env.API_KEY_VALUE
 const apiURL = process.env.API_BASE_URL
-const devENV = process.env.NODE_ENV == 'development'
+const devENV = process.env.NODE_ENV == 'dev'
 
 // Config Cross Origin Resource Sharing
 app.use(cors({
     origin: process.env.APP_URL
 }))
 
+/*
+    Login
+    Params:
+        user <string> - required
+        password <string> - required
+*/
 app.post('/login', function(req, res) {
-    // Verifiy that request has all paremeters
+    // Check request paremeters
     if (!req.query.user || !req.query.password) {returnError(res, 400, 'Bad Request'); return}
 
+    // Compare credentials with dummy login
     const auth = req.query
 
-    // Compare credentials with dummy login
     if (auth.user == process.env.AUTH_USER && auth.password == process.env.AUTH_PASSWORD) {
         res.json({
             token: generateAccessToken()
@@ -33,11 +39,16 @@ app.post('/login', function(req, res) {
     }
 })
 
+/*
+    Currencies
+    Params:
+        none
+*/
 app.get('/currencies', function(req, res, next) { // get currency
     // Check token's validation
     if (!checkAccessToken(req) && !devENV) {returnError(res, 403, 'Unauthorized'); return}
 
-    // Delete token for local validation
+    // Apply format to parameters
     delete req.query.token
 
     // Create string with parameters and add API Token
@@ -50,6 +61,38 @@ app.get('/currencies', function(req, res, next) { // get currency
     .catch(err => next(err))
 })
 
+/*
+    Historic
+    Params:
+        base <string> - optional // Base currency, default is USD - only available for paid plans
+        symbols <string[]> - optional // Limit return currencies list
+        date <string (yyyy-mm-dd)> - optional // Get specific date exchage rates
+        dates <string[] (yyyy-mm-dd)> - optional // Get exchange rates from multiple dates
+*/
+app.get('/historic', function(req, res, next) {
+    // Check token's validation
+    if (!checkAccessToken(req) && !devENV) {returnError(res, 403, 'Unauthorized'); return}
+
+    // Apply format to parameters
+    delete req.query.token
+
+    // Create string with parameters and add API Token
+    const qParams = req.query
+    const params = new URLSearchParams()
+    params.append('app_id', apiKEY)
+    if (qParams.base) params.append('base', qParams.base)
+    if (qParams.symbols) params.append('symbols', qParams.symbols)
+
+    const dates = qParams.date ? [qParams.date] : qParams.dates
+
+    let fetchBuffer = []
+
+    for(let date of dates) {
+        fetchBuffer.push(fetch(`${apiURL}/historical/${date}.json?${params}`).then(resp => resp.json()))
+    }
+
+    Promise.all(fetchBuffer).then(dataList => res.send(dataList))
+})
 
 // Send non existent GET request to 404
 app.get('*', function(req, res){
